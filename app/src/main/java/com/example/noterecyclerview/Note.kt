a5f586e6975d1778207a614e7f3347bac850935e
Nasser Alawi <duniaalahlam2016@gmail.com>
2021-05-14 04:55:21 +0300

    add priority to cardView

--- app/src/main/java/com/example/noterecyclerview/Note.kt
+++ app/src/main/java/com/example/noterecyclerview/Note.kt
@@ -2,5 +2,6 @@ package com.example.noterecyclerview
 
 data class Note (val imageResource: Int ,
                  val note1Text:String ,
-                 val note2Text:String
+                 val note2Text:String ,
+                 val notePriority : Int
 )
--- app/src/main/java/com/example/noterecyclerview/NoteAdapter.kt
+++ app/src/main/java/com/example/noterecyclerview/NoteAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.noterecyclerview
 
+import android.graphics.Color
 import android.util.Log.i
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,7 @@ class NoteAdapter(private val notesList: List<Note>) : RecyclerView.Adapter<Note
         val imageView:ImageView = itemView.findViewById(R.id.image_view)
         val text1: TextView = itemView.findViewById(R.id.not1_text_view)
         val text2: TextView = itemView.findViewById(R.id.not2_text_view)
+        val priorityText:TextView = itemView.findViewById(R.id.priority_text_view)
 
     }
 
@@ -25,7 +27,15 @@ class NoteAdapter(private val notesList: List<Note>) : RecyclerView.Adapter<Note
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
        val currentNote = notesList[position]
-        holder.imageView.setImageResource(currentNote.imageResource)
+        if (currentNote.notePriority > 5){
+            holder.priorityText.setTextColor(Color.RED)
+            holder.imageView.setImageResource(currentNote.imageResource)
+        } else {
+            holder.priorityText.setTextColor(Color.BLACK)
+            holder.imageView.setImageResource(R.drawable.ic_time_blue)
+
+        }
+        holder.priorityText.text = currentNote.notePriority.toString()
         holder.text1.text = currentNote.note1Text
         holder.text2.text = currentNote.note2Text
 
