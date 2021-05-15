699a6b0049e0713470291132f2cba300c481a0e4
Nasser Alawi <duniaalahlam2016@gmail.com>
2021-05-15 03:17:42 +0300

    add and remove note to recyclerView

--- app/src/main/java/com/example/noterecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/noterecyclerview/MainActivity.kt
@@ -5,17 +5,30 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
-   private var  notesList = ArrayList<Note>()
+   private val  notesList = ArrayList<Note>()
+    private val adapter = NoteAdapter(notesList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        insert_btn.setOnClickListener {
+            val newItem = Note(R.drawable.ic_time ,"new item", "details", Random.nextInt(10))
+            notesList.add(newItem)
+            adapter.notifyItemInserted(insert_edit_text.text.toString().toInt())
+        }
+        remove_btn.setOnClickListener {
+         val index=  remove_edit_text.text.toString().toInt()
+            notesList.removeAt(index)
+            adapter.notifyItemRemoved(index)
+        }
         buildArrayList()
         buildRecyclerView()
-        
+
 
     }
 
@@ -24,19 +37,16 @@ class MainActivity : AppCompatActivity() {
         notesList.add(Note(R.drawable.ic_time , "note1" , "note2" , 6))
         notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 7))
         notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 3))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 1))
-        notesList.add(Note(R.drawable.ic_time , "note81" , "note882", 10))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 8))
         notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 7))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2",9))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 6))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 9))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 2))
-        notesList.add(Note(R.drawable.ic_time , "note1" , "note2" , 4))
+        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 3))
+        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 7))
+        notesList.add(Note(R.drawable.ic_time , "note1" , "note2", 3))
+
+
     }
 
     private fun buildRecyclerView(){
-        notes_recycler_view.adapter = NoteAdapter(notesList)
+        notes_recycler_view.adapter = adapter
         notes_recycler_view.layoutManager = LinearLayoutManager(this)
         notes_recycler_view.setHasFixedSize(true)
     }
